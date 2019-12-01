c2c6733bdb4079b6021368ae7e7aca2ac0810093
src-A- <51412550+srcee@users.noreply.github.com>
2019-12-01 15:45:52 +0200

    Update app.js

--- Team-Manager-Skeleton/scripts/app.js
+++ Team-Manager-Skeleton/scripts/app.js
@@ -1,26 +1,23 @@
 import  *  as  ctrl  from "./controllers.js";
 
-(() => {
-    const app = Sammy('#main', function () {
-        this.use('Handlebars', 'hbs');
-        //home and about
-        this.get('#/', ctrl.loadHome);
-        this.get('#/home', ctrl.loadHome);
-        this.get('#/about', ctrl.getAboutController);
-        //login and logout
-        this.get('#/login', ctrl.getLoginController);
-        this.post('#/login', ctrl.postLoginController);
-        this.get('#/logout', ctrl.getLogoutController);
-        //register
-        this.get('#/register', ctrl.getRegisterController);
-        this.post('#/register', ctrl.postRegisterController);
-        //catalog and teams details
-        this.get('#/catalog', ctrl.getCatalogController)
-        this.get('#/create', ctrl.getCreateController)
-        this.post('#/create', ctrl.postCreateController);
-        this.get('#/catalog/:teamId', ctrl.getTeamInfoController);
-        this.get('#/join/:teamId', ctrl.getJoinController);
-        this.get('#/leave/:teamId', ctrl.getLeaveController)        
-    });
-    app.run();
-})()
+const app = Sammy('#main', function () {
+   this.use('Handlebars', 'hbs');
+   //home and about
+   this.get('#/', ctrl.loadHome);
+   this.get('#/home', ctrl.loadHome);
+   this.get('#/about', ctrl.getAboutController);
+   //login and logout
+   this.get('#/login', ctrl.getLoginController);
+   this.post('#/login', ctrl.postLoginController);
+   this.get('#/logout', ctrl.getLogoutController);
+   //register
+   this.get('#/register', ctrl.getRegisterController);
+   this.post('#/register', ctrl.postRegisterController);
+   //catalog and teams details
+   this.get('#/catalog', ctrl.getCatalogController)
+   this.get('#/create', ctrl.getCreateController)
+   this.post('#/create', ctrl.postCreateController);
+   this.get('#/catalog/:teamId', ctrl.getTeamInfoController);
+   this.get('#/join/:teamId', ctrl.getJoinController);
+   this.get('#/leave/:teamId', ctrl.getLeaveController)        });
+app.run();
